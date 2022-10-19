f88ccf0a4a486f7a86b39f180ee739bd9a062d1a
Albert Valls <avalls89@gmail.com>
2022-10-19 19:14:53 +0200

    Add tree directories

---
+++ src/components/Actions/Actions.tsx
@@ -0,0 +1,14 @@
+const Actions = (): JSX.Element => {
+  return (
+    <>
+      <a href="#" className="call">
+        Call
+      </a>
+
+      <a href="#" className="hang active">
+        Hang
+      </a>
+    </>
+  );
+};
+export default Actions;
